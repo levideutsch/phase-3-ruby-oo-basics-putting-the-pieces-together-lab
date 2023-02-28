a96f851e04fc84fb7c9fc3169496b478cfd4c789
levideutsch <levideutsch@live.com>
2023-02-28 00:32:09 -0500

    commit tree 815a1c779ef7ef954c40e8cbedf4fadebe4b899e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+require 'pry'
 
+class Book
+
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+
+    def initialize(title, author="Agatha Christie", page_count=272, genre="Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end 
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end 
+
+new_book = Book.new("And Then There Were None")
+# page_count = Book.new(272)
+# new_author = Book.new("Agatha Christie")
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe 
+
+
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand, color="red", size=9.5, material="suede", condition="tattered")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+   
+end 
+
+    shoe = Shoe.new("Adidas")
+    # binding.pry
+    # brand.condition = "new"
+  
